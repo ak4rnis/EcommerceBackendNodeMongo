1c993c98e03db58f8101eeb89dd0f2fe8183cdc1
ak4rnis <pieero.flores500@gmail.com>
2023-04-14 15:42:00 -0400

    crear-filtrar-cupones-backend

--- routes/cupon.js
+++ routes/cupon.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cuponController = require("../controllers/CuponController");
 const api = express.Router();
+const auth = require("../middlewares/authenticate");
+
+api.post('/registro_cupon_admin',auth.auth, cuponController.registro_cupon_admin);
+api.get('/listar_cupones_admin/:filtro?',auth.auth, cuponController.listar_cupones_admin);
 
 module.exports = api;
